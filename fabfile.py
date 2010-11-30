779fd4c83cd15222388adcaf1624b90ef0ba18d9
razamobin <raza.mobin@gmail.com>
2010-11-30 01:25:35 -0800

    latest fab file plus todo update

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,13 @@
 from fabric.api import *
+import fabric
 
 env.user = 'ubuntu'
 env.hosts = ['sugarlog.com']
 env.key_filename = '/home/rmobin/.ssh/ec2-sample-key.pem'
 
 def deploy():
+    msg = fabric.operations.prompt('commit message?')
+    local('git commit -a -m "%s"' % msg)
     local('tar czf /tmp/sugarlog.tgz sugarlog.py static templates')
     put('/tmp/sugarlog.tgz', '/tmp/')
     with cd('/mnt/sda/sugarlog'):
