3db6f7ca3d5e22bb363c7372cdbffbbf4f28bb2c
razamobin <raza.mobin@gmail.com>
2010-11-30 01:30:08 -0800

    updated fabfile to do git commit/push

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,18 @@
 from fabric.api import *
 import fabric
+from fabric.contrib.console import confirm
 
 env.user = 'ubuntu'
 env.hosts = ['sugarlog.com']
 env.key_filename = '/home/rmobin/.ssh/ec2-sample-key.pem'
 
 def deploy():
-    msg = fabric.operations.prompt('commit message?')
+    local('git status -s')
+    if not confirm('commit above changes?'):
+        abort('aborting deploy at your request')
+    msg = fabric.operations.prompt('enter the commit message:')
     local('git commit -a -m "%s"' % msg)
+    local('git push origin master')
     local('tar czf /tmp/sugarlog.tgz sugarlog.py static templates')
     put('/tmp/sugarlog.tgz', '/tmp/')
     with cd('/mnt/sda/sugarlog'):
