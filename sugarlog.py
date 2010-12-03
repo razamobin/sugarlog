4888d8d22d014be655cee88d1e5827f59b87528f
razamobin <raza.mobin@gmail.com>
2010-12-03 01:20:53 -0800

    added primitive support for comments

--- sugarlog.py
+++ sugarlog.py
@@ -25,10 +25,13 @@ def after_request(response):
 def main():
     days = []
     entries = {}
+    # map entry ids to list of comment hashes
+    commentsHash = {}
     entry_data = g.redis.sort('sugarlog:entries', 0, 1000, 'sugarlog:entry:*->sort',
         ['sugarlog:entry:*->day', 'sugarlog:entry:*->time',
-        'sugarlog:entry:*->blood_sugar', 'sugarlog:entry:*->notes'], True)
-    for i in range(0, len(entry_data), 4):
+        'sugarlog:entry:*->blood_sugar', 'sugarlog:entry:*->notes', '#'], True)
+
+    for i in range(0, len(entry_data), 5):
         day = day_str(time.strptime(entry_data[i], '%Y-%m-%d'))
         if day in entries:
             day_entries = entries[day]
@@ -40,9 +43,25 @@ def main():
         day_entries.append({'day' : entry_data[i], 
             'time' : entry_data[i+1], 
             'blood_sugar' : entry_data[i+2], 
-            'notes' : entry_data[i+3]}) 
+            'notes' : entry_data[i+3],
+            'entry_id' : entry_data[i+4]}) 
 
-    return render_template('index.html', days=days, entries=entries)
+        entry_id = entry_data[i+4]
+        # fetch comments for this entry
+        comment_data = g.redis.sort('sugarlog:entry:%s:comments' % entry_id,
+            0, 100, 'sugarlog:comment:*->timestamp', 
+            ['sugarlog:comment:*->author', 'sugarlog:comment:*->comment'], True)
+        if comment_data:
+            # iterate with steps of 2
+            for i in range(0, len(comment_data), 2):
+                if entry_id in commentsHash:
+                    comments = commentsHash[entry_id]
+                else:
+                    comments = []
+                    commentsHash[entry_id] = comments
+                comments.append({'author' : comment_data[i], 'comment' : comment_data[i+1]})
+
+    return render_template('index.html', days=days, entries=entries, commentsHash=commentsHash)
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
@@ -67,6 +86,26 @@ def new():
     else:
         return render_template('new.html', user={}, day_display=today_str(), day_input=time.strftime('%Y-%m-%d'))
 
+@app.route('/comments', methods=['GET', 'POST'])
+def comments():
+    if request.form.keys():
+        comment_hash = {
+            'timestamp' : int(time.time()),
+            'author' : request.form.get('author'),
+            'comment' : request.form.get('comment')}
+        # get next comment id
+        comment_id = g.redis.incr('global:next_comment_id')
+
+        # create hash entry with the comment data
+        g.redis.hmset('sugarlog:comment:%s' % comment_id, comment_hash)
+
+        # append list of ids
+        g.redis.lpush('sugarlog:entry:%s:comments' % request.form.get('entry_id'), comment_id)
+
+        return redirect(url_for('main'))
+    else:
+        return render_template('comments.html')
+
 def day_str(time_struct):
     s = time.strftime('%B %e', time_struct).lower()
     i = int(re.compile('\s+').split(s)[1]);
@@ -80,16 +119,7 @@ def day_str(time_struct):
         return s + 'th'
 
 def today_str():
-    s = time.strftime('%B %e').lower()
-    i = int(re.compile('\s+').split(s)[1]);
-    if i in [1,21,31]:
-        return s + 'st'
-    elif i in [2,22] :
-        return s + 'nd'
-    elif i in [3,23] :
-        return s + 'rd'
-    else:
-        return s + 'th'
+    return day_str(time.gmtime())
 
 if __name__ == '__main__':
     app.run()
